6b4ee22c3d16ab965f7e2e1598df4e24d1f99ce9
LudvigThunberg <ludvigthunberg@gmail.com>
2022-01-03 17:00:13 +0100

    added go button functionallity

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -5,6 +5,8 @@ window.onload = function () {
     "create-go-button"
   ) as HTMLButtonElement;
   goButton.addEventListener("click", handleGoButton);
+
+  getFromLocalStorage();
 };
 
 let blogers: Bloger[] = [];
@@ -34,9 +36,45 @@ function handleGoButton() {
   );
   blogers.push(createBloger);
   sendToLocalStorage();
+  goButtonNextPage();
+  console.log(blogers);
 }
 
 function sendToLocalStorage() {
   let blogersLS = JSON.stringify(blogers);
   localStorage.setItem("blogers", blogersLS);
 }
+
+function getFromLocalStorage() {
+  let blogersLS = localStorage.getItem("blogers");
+  if (!blogersLS) {
+    sendToLocalStorage();
+  } else {
+    blogersLS = localStorage.getItem("blogers");
+    blogers = JSON.parse(blogersLS);
+  }
+}
+
+function goButtonNextPage() {
+  window.location.href = "selectBloger.html";
+}
+
+/* function emptyInputs() {
+  let createAuthorInput: HTMLInputElement = document.getElementById(
+    "create-author-input"
+  ) as HTMLInputElement;
+  let createBlogTitleInput: HTMLInputElement = document.getElementById(
+    "create-blog-name-input"
+  ) as HTMLInputElement;
+  let createBlogDescriptionInput: HTMLInputElement = document.getElementById(
+    "create-blog-description-input"
+  ) as HTMLInputElement;
+  let createBlogImgUrl: HTMLInputElement = document.getElementById(
+    "create-blog-img-url"
+  ) as HTMLInputElement;
+
+  createAuthorInput.innerHTML = "";
+  createBlogTitleInput.innerHTML = "";
+  createBlogDescriptionInput.innerHTML = "";
+  createBlogImgUrl.innerHTML = "";
+} */
