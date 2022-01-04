d53fc7069e29a0bb6fdf376d47005bbf55ceac09
LudvigThunberg <ludvigthunberg@gmail.com>
2022-01-04 15:13:55 +0100

    added create text/img-area and some styling

--- src/ts/createArticle.ts
+++ src/ts/createArticle.ts
@@ -7,7 +7,25 @@ window.onload = function () {
   getFromLocalStorage();
   getFromSessionStorage();
   findCorrectBlogger();
+  eventListeners();
 };
+
+function eventListeners() {
+  let createArticleTextFieldButton: HTMLButtonElement = document.getElementById(
+    "createArticle-text-field-button"
+  ) as HTMLButtonElement;
+  createArticleTextFieldButton.addEventListener(
+    "click",
+    createTextInputWithButtonClick
+  );
+  let createArticleImageFieldButton: HTMLButtonElement = document.getElementById(
+    "createArticle-image-field-button"
+  ) as HTMLButtonElement;
+  createArticleImageFieldButton.addEventListener(
+    "click",
+    createImageInputWithButtonClick
+  );
+}
 
 function sendToLocalStorage() {
   let blogersLS = JSON.stringify(blogers);
@@ -33,4 +51,23 @@ function findCorrectBlogger() {
     (theCreator) => theCreator.id.toString() === blogerIdValue
   );
   console.log(blogCreator);
+}
+
+function createTextInputWithButtonClick() {
+  let blogPostsContainer: HTMLDivElement = document.getElementById(
+    "blog-posts-container"
+  ) as HTMLDivElement;
+  let newTextArea = document.createElement("textarea");
+  newTextArea.className = "new-textarea";
+  blogPostsContainer.appendChild(newTextArea);
+}
+
+function createImageInputWithButtonClick() {
+  let blogPostsContainer: HTMLDivElement = document.getElementById(
+    "blog-posts-container"
+  ) as HTMLDivElement;
+  let newImageInput = document.createElement("input");
+  newImageInput.className = "new-image-input";
+  newImageInput.type = "text";
+  blogPostsContainer.appendChild(newImageInput);
 }
